69c8431e541094451f95c328200f5a13790d6a0d
zurachu <zurachu@gmail.com>
2021-12-24 23:08:42 +0900

    AudioManager_KanKikuchi

---
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -0,0 +1,11 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// BGMファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class BGMPath{
+
+
+}
+
+}
---
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -0,0 +1,11 @@
+﻿namespace KanKikuchi.AudioManager{
+
+/// <summary>
+/// SEファイルへのパスを定数で管理するクラス
+/// </summary>
+public static class SEPath{
+
+
+}
+
+}
